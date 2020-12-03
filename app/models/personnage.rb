0030dc29013f0b24e512273ff183362c3e0a5c33
PierrickSTAES <pstaes.sta@gmf.fr>
2020-12-03 10:07:57 +0100

    search v3

--- app/models/personnage.rb
+++ app/models/personnage.rb
@@ -1,5 +1,9 @@
 class Personnage < ApplicationRecord
 	has_many :objets, dependent: :destroy
 	include PgSearch  
-	pg_search_scope :search, against: [:name, :niveau, :personnage_class]
+	pg_search_scope :search, 
+					against: [:name, :niveau, :personnage_class], 
+					using: { 
+						tsearch: { prefix: true }
+					}
 end
